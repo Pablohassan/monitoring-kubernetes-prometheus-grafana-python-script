460ecd340e7023fcb429caa512bf11aab00cd404
arno-yv <152905319+arno-yv@users.noreply.github.com>
2024-02-07 15:38:46 +0100

    Update main.py - add line in bed

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ async def startup():
         await database.connect()
     # create a dummy entry
     await User.objects.get_or_create(email="test@test.com")
-    #await User.objects.get_or_create(email="coucou@datascientest.com")
+    await User.objects.get_or_create(email="coucou@datascientest.com")
     #await User.objects.get_or_create(email="coucou@antoine.com")
 
 
