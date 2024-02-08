74e5055c5e5e11cdbc7c627979c4afabb33f64e4
Sadikovic Rusmir <sesmanovic@gmail.com>
2024-02-08 01:21:54 +0100

    rebase

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
-from fastapi import FastAPI, Request
-from prometheus_client import Counter, make_asgi_app
+from fastapi import FastAPI, Request, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 from starlette.middleware.base import BaseHTTPMiddleware
 from app.db import database, User
 
-
 app = FastAPI(title="FastAPI, Docker, and Traefik")
 
-#Prometheus metrics
+# Prometheus metrics
 nb_of_requests_counter = Counter(
-    'nb_of_requests', 
+    'nb_of_requests',
     'Number of successful requests made to the app',
     ['method', 'endpoint']
 )
@@ -19,20 +18,24 @@ exceptions_counter = Counter(
     []
 )
 
+
 class MetricsMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         endpoint = request.url.path
         method = request.method
         try:
             response = await call_next(request)
-            nb_of_requests_counter.labels(method=method, endpoint=endpoint).inc()
+            nb_of_requests_counter.labels(
+                method=method, endpoint=endpoint).inc()
             return response
         except Exception as e:
             exceptions_counter.inc()
             raise e from None
 
+
 # Add the middleware to the app
 app.add_middleware(MetricsMiddleware)
+
 
 @app.get("/")
 async def read_root():
@@ -49,10 +52,12 @@ async def startup():
     await User.objects.get_or_create(email="coucou@antoine.com")
 
 
-    # Add /metrics route for Prometheus scraping
-    app.add_route("/metrics", make_asgi_app())
-
 @app.on_event("shutdown")
 async def shutdown():
     if database.is_connected:
         await database.disconnect()
+
+
+@app.get("/metrics")
+async def get_metrics():
+    return Response(content=generate_latest(), media_type=CONTENT_TYPE_LATEST)
